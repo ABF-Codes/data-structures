c206b610434edfa54017a39889f305b082ceae44
Abdullah Bin Faheem <abdullahbinfaheem31@gmail.com>
2026-09-08 03:15:56 +0500

    basic of ll

--- Linked List/singleLinkedList.cpp
+++ Linked List/singleLinkedList.cpp
@@ -1,26 +1,23 @@
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 
-struct node{
-    int data;
-    node* next;
+class Node{
+    public:
+
+    int value;
+    Node* next;
+
+    
+    Node(int data){
+        value=data;
+        next=NULL;
+    }
+
 };
-
 int main(){
-    node* first = new node();
-    node* second = new node();
-    node* third = new node();
 
-     first->data=10;
-     second->data=20;
-     third->data=30;
-
-     first->next=second;
-     second->next=third;
-     third->next=nullptr;
-
-     cout<<"First Node Data : "<<first->data<<" , "<< "Address of first Node :"<<first<<" , "<<"First node points to :"<<first->next<<endl;
-     cout<<"Second Node Data : "<<second->data<<" , "<< "Address of 2nd Node :"<<second<<" , "<<"Second node points to :"<<second->next<<endl;
-
+    Node* n = new Node(1);
+    cout<<n->value<<" , "<<n->next<<endl;
     return 0;
 }
