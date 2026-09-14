6cf9826e5efd7449e1e9c035a25274a2ffadceeb
Abdullah Bin Faheem <abdullahbinfaheem31@gmail.com>
2026-09-14 17:21:31 +0500

    Repractice

--- Linked List/prcatice.cpp
+++ Linked List/prcatice.cpp
@@ -3,20 +3,18 @@ using namespace std;
 
 class Node{
 public:
-    int value;
+    int data;
     Node* next;
 
-    Node(int valu){
-        value=valu;
+    Node(int value){
+        data= value;
         next=NULL;
     }
 };
-
 class LList{
 private:
     Node* head;
     Node* tail;
-
 public:
     LList(){
         head=NULL;
@@ -25,33 +23,30 @@ public:
     void push_front(int val){
         Node* newNode = new Node(val);
         if(head == NULL){
-            head=tail=newNode;
-            return ;
-        
+            head = tail = newNode;
         }
         else{
-          newNode->next = head;
-          head = newNode;
+            newNode->next = head;
+            head = newNode;
         }
 
     }
+    
     void print_list(){
         Node* temp = head;
         while(temp != nullptr){
-            cout<<temp->value<<"->";
-            temp=temp->next;
+            cout<<temp->data<<"->";
+            temp= temp->next;
         }
         cout<<"NULL";
     }
-
 };
-
 int main(){
     LList l1;
     l1.push_front(1);
     l1.push_front(2);
     l1.push_front(3);
-    l1.push_front(4);
+    l1.push_front(6);
     l1.print_list();
     return 0;
 }
